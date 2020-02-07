b4c2f3d51b9d5764222ba77c6c2b28bb9a4a5918
zhoushibo <1216147314@qq.com>
2020-02-07 20:38:37 +0800

    显示焦点图

--- bigevent/web_front/js/util/article.js
+++ bigevent/web_front/js/util/article.js
@@ -1,12 +1,10 @@
 // 把所有与文章相关的操作写在这里
 
 var article = {
-    // 获取
-    // page 就是当前要取出来的第几页的数据
-    // type 就是当前文章的类型编号
-    // status 就是当前文章的状态
-    get: function(page, type, status) {
-        return $.get(APILIST.article_get, { 'page': page, 'type': type, 'state': status })
+    // 获取主页中5条焦点图新闻
+    getFouceFive: function() {
+        // 因为只要5条数据 并显示
+        return $.get(APILIST.article_get, { perpage: 5 })
     },
     /**
      * 
@@ -15,30 +13,6 @@ var article = {
     getById: function(id) {
         return $.get(APILIST.article_get, { 'id': id })
     },
-    // 删除文章
-    del: function(id) {
-        return $.get(APILIST.article_del, { 'id': id })
-    },
-    // 添加
-    add: function(fd) {
-        return $.ajax({
-            url: APILIST.article_add,
-            type: 'post',
-            data: fd,
-            processData: false, //不允许处理数据
-            contentType: false, //不要设置请求头
-        })
-    },
-    //编辑并保存 
-    edit: function(fd) {
-        return $.ajax({
-            type: 'post',
-            url: APILIST.article_edit,
-            data: fd,
-            processData: false, // 不要让jquery去处理formData数据
-            contentType: false, // 不设置默认的请求头
 
-        })
-    }
 
 }
